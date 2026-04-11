52459544efa72375d6826247d0c692fdd30ddecc
limikyoung1006-blip <limikyoung1006@gmail.com>
2026-04-11 21:19:16 +0900

    Update index.ts

--- supabase/functions/send-push/index.ts
+++ supabase/functions/send-push/index.ts
@@ -15,6 +15,7 @@ if (VAPID_PRIVATE_KEY) {
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
 }
 
 serve(async (req) => {
